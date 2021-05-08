b672c95a91d448a67b2b6f4661a9465cea43d299
unknown <waly@gmail.com>
2021-05-08 17:52:27 +0200

    Updated Controller

--- MovieService/src/main/java/pl/pjatk/MovieService/controller/MovieController.java
+++ MovieService/src/main/java/pl/pjatk/MovieService/controller/MovieController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/movieservice")
+@RequestMapping("/moviesController")
 public class MovieController {
 
    private final MovieService movieService;
