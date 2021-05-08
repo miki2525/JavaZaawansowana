2ade20968a52d34a7883ea3e78fad65e6322e68d
unknown <waly@gmail.com>
2021-05-08 17:17:26 +0200

    Controller

--- MovieService/src/main/java/pl/pjatk/MovieService/controller/MovieController.java
+++ MovieService/src/main/java/pl/pjatk/MovieService/controller/MovieController.java
@@ -3,18 +3,20 @@ package pl.pjatk.MovieService.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.pjatk.MovieService.model.Movie;
 import pl.pjatk.MovieService.service.MovieService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/movieservice")
 public class MovieController {
 
-    MovieService movieService;
+   private final MovieService movieService;
 
     public MovieController(MovieService movieService){
         this.movieService = movieService;
@@ -26,4 +28,15 @@ public class MovieController {
     return ResponseEntity.ok(movieService.getAllMovies());
     }
 
+
+    @GetMapping ("/movies/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable Long id){
+        Optional<Movie> movie = movieService.getMovieById(id);
+        if (movie.isPresent()){
+            return ResponseEntity.ok(movie.get());
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- MovieService/src/main/java/pl/pjatk/MovieService/service/MovieService.java
+++ MovieService/src/main/java/pl/pjatk/MovieService/service/MovieService.java
@@ -5,6 +5,7 @@ import pl.pjatk.MovieService.model.Movie;
 import pl.pjatk.MovieService.model.MovieCategory;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -16,5 +17,18 @@ public class MovieService {
                 new Movie(3l, "LordOfTheRings", MovieCategory.Fantasy),
                 new Movie(4l, "XXX", MovieCategory.Action)
                 );
+    }
+
+    public Optional<Movie> getMovieById(Long id){
+        List<Movie> movieList = List.of(
+                new Movie(1l, "Scream", MovieCategory.Horror),
+                new Movie(2l, "SpaceJam", MovieCategory.Comedy),
+                new Movie(3l, "LordOfTheRings", MovieCategory.Fantasy),
+                new Movie(4l, "XXX", MovieCategory.Action)
+        );
+
+        return movieList.stream().
+                filter(movie -> movie.getId() == id).findFirst();
+
     }
 }
