9746201be3245a66d72e66316e2d0e3870cf2212
unknown <waly@gmail.com>
2021-05-08 17:48:28 +0200

    Updated Controller + Service

--- MovieService/src/main/java/pl/pjatk/MovieService/controller/MovieController.java
+++ MovieService/src/main/java/pl/pjatk/MovieService/controller/MovieController.java
@@ -1,12 +1,11 @@
 package pl.pjatk.MovieService.controller;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.pjatk.MovieService.model.Movie;
+import pl.pjatk.MovieService.model.MovieCategory;
 import pl.pjatk.MovieService.service.MovieService;
 
 import java.util.List;
@@ -39,4 +38,40 @@ public class MovieController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    @PostMapping("/movies")
+    public ResponseEntity<Movie> createNewMovie(@RequestBody Movie movie){
+        if (movie.getId() instanceof Long &&
+        movie.getName() instanceof String &&
+        movie.getCategory() instanceof MovieCategory) {
+
+            return ResponseEntity.ok(movie);
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+        /////if cannot persist to db return 404
+    }
+
+    @PutMapping ("/movies/{id}")
+    public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie updateMovie) {
+        Optional<Movie> movie = movieService.getMovieById(id);
+        if (movie.isPresent()) {
+            movie.get().setName(updateMovie.getName());
+            movie.get().setCategory(updateMovie.getCategory());
+            return ResponseEntity.ok(movie.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/movies/{id}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable Long id){
+        if (movieService.deleteById(id)){
+            return new ResponseEntity(HttpStatus.OK);
+        }
+        else return ResponseEntity.notFound().build();
+    }
+
+
 }
--- MovieService/src/main/java/pl/pjatk/MovieService/service/MovieService.java
+++ MovieService/src/main/java/pl/pjatk/MovieService/service/MovieService.java
@@ -4,31 +4,35 @@ import org.springframework.stereotype.Service;
 import pl.pjatk.MovieService.model.Movie;
 import pl.pjatk.MovieService.model.MovieCategory;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class MovieService {
 
-    public List<Movie> getAllMovies(){
-        return List.of(
+    private List<Movie> movieList = new ArrayList<>(List.of(
                 new Movie(1l, "Scream", MovieCategory.Horror),
                 new Movie(2l, "SpaceJam", MovieCategory.Comedy),
                 new Movie(3l, "LordOfTheRings", MovieCategory.Fantasy),
-                new Movie(4l, "XXX", MovieCategory.Action)
-                );
+                new Movie(4l, "XXX", MovieCategory.Action)));
+
+    public List<Movie> getAllMovies(){
+        return movieList;
     }
 
     public Optional<Movie> getMovieById(Long id){
-        List<Movie> movieList = List.of(
-                new Movie(1l, "Scream", MovieCategory.Horror),
-                new Movie(2l, "SpaceJam", MovieCategory.Comedy),
-                new Movie(3l, "LordOfTheRings", MovieCategory.Fantasy),
-                new Movie(4l, "XXX", MovieCategory.Action)
-        );
+        return movieList.stream()
+                .filter(movie -> movie.getId() == id)
+                .findAny();
+    }
 
-        return movieList.stream().
-                filter(movie -> movie.getId() == id).findFirst();
-
+    public Boolean deleteById(Long id){
+        Optional<Movie> movie = getMovieById(id);
+        if (movie.isPresent()){
+            movieList.remove(movie);
+            return true;
+        }
+        return false;
     }
 }
