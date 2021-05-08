d154fec344ede1c5ba2b9ec53c08dabdc061c221
unknown <waly@gmail.com>
2021-05-08 16:39:40 +0200

    create Model

---
+++ MovieService/src/main/java/pl/pjatk/MovieService/model/Movie.java
@@ -0,0 +1,67 @@
+package pl.pjatk.MovieService.model;
+
+import java.util.Objects;
+
+public class Movie {
+
+    Long id;
+    String name;
+    MovieCategory category;
+
+    public Movie(Long id, String name, MovieCategory category) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+    }
+
+    public Movie(String name, MovieCategory category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public MovieCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(MovieCategory category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category=" + category +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return Objects.equals(name, movie.name) && category == movie.category;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, category);
+    }
+}
---
+++ MovieService/src/main/java/pl/pjatk/MovieService/model/MovieCategory.java
@@ -0,0 +1,11 @@
+package pl.pjatk.MovieService.model;
+
+public enum MovieCategory {
+    Horror,
+    Fantasy,
+    Comedy,
+    Romance,
+    Action,
+    Drama,
+    Thriller
+}
