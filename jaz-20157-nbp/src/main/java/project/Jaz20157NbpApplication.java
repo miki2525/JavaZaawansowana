7a5c5334d08cb67ea977120372db02b700c1bd44
unknown <waly@gmail.com>
2021-07-03 16:20:44 +0200

    create new project

---
+++ jaz-20157-nbp/src/main/java/project/Jaz20157NbpApplication.java
@@ -0,0 +1,13 @@
+package project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaz20157NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jaz20157NbpApplication.class, args);
+	}
+
+}
